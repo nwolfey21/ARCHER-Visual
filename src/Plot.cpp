7fa08a7060faafa6d0f6f07176c61e5307e9e1a7
Noah Wolfe <wolfen@rpi.edu>
2014-06-19 16:56:23 -0400

    added light functionality

--- src/Plot.cpp
+++ src/Plot.cpp
@@ -1,21 +1,10 @@
 /*******************************************************************
-
-Rensselaer Polytechnic Institute
-
+                  Rensselaer Polytechnic Institute
 ********************************************************************
-
 Discription:
-  functions for plotting and rendering the surfaces
-
-Version: 1.0
-
-Currently: 04/19/2014
-  Initial Code base
-
+  functions for plotting and rendering objects and particles
 *******************************************************************/
-
-//Includes, System
-// includes
+/* Class Header */
 #include "classes.h"
 /* OpenGL Code Headers */
 #include <GL/glui.h>
@@ -35,7 +24,7 @@ void drawOBJs(meshOBJ *obj)
 		if(obj[i].isValidated())
 		{
 //			glColor3f(0.0, 0.0, 1.5*i);
-			glColor4f(.23,.78,.32,0.1);
+			glColor4f(.8,.78,.32,0.1);
 			for(unsigned int j=0;j<obj[i].getSize();j++)
 			{
 				normal = obj[i].getNormals(j);
@@ -72,15 +61,9 @@ void drawParticles(particle *particles)
 			glTranslatef(v.getX(),v.getZ(),v.getY());
 			glutSolidSphere (0.5, 20, 16);
 			glPopMatrix();
-			//Render 1 Face
-//			printf("obj:%d face:%d normal(%f %f %f) vertex(%f %f %f) \n",i,j,normal[0].getX(),normal[0].getY(),normal[0].getZ(),vertex[0].getX(),vertex[0].getY(),vertex[0].getZ());
-/*			glNormal3f(normal[0].getX(),normal[0].getY(),normal[0].getZ());
-			glVertex3f(vertex[0].getX(),vertex[0].getY(),vertex[0].getZ());
-			glNormal3f(normal[1].getX(),normal[1].getY(),normal[1].getZ());
-			glVertex3f(vertex[1].getX(),vertex[1].getY(),vertex[1].getZ());
-			glNormal3f(normal[2].getX(),normal[2].getY(),normal[2].getZ());
-			glVertex3f(vertex[2].getX(),vertex[2].getY(),vertex[2].getZ());
-*/		}
+		}
+//Trying to connect dots with lines
+/*
 		glBegin(GL_LINES); 
 	  	glLineWidth(4.0);
 		for(unsigned int j=0;j<particles[i].getSize();j++)
@@ -90,6 +73,6 @@ void drawParticles(particle *particles)
 //			glVertex3f(v.getX(),v.getY(),v.getZ());
 		}
 		glEnd();
-	}
+*/	}
 //	glPopMatrix();
 }
