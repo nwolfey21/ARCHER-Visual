d9058dc79c42a355c61310b3d6329d1ad4374856
Noah Wolfe <wolfen@rpi.edu>
2014-06-23 16:00:03 -0400

    animates first particle

--- src/Plot.cpp
+++ src/Plot.cpp
@@ -14,10 +14,8 @@ void drawOBJs(meshOBJ *obj, float *colorTable, int colored)
 	printf("drawing Objects\n");
 	glPushMatrix(); // GL_MODELVIEW is default
 	glScalef(1.0 / 100.0, 1.0 / 100.0, 1.0/100.0);
-
 	vertex* normal;
 	vertex* vertex;
-
 	glBegin(GL_TRIANGLES); 
 	for(int i=0;i<138;i++)
 	{
@@ -53,15 +51,14 @@ void drawOBJs(meshOBJ *obj, float *colorTable, int colored)
 void drawParticles(particle *particles)
 {
 	printf("drawing Particles\n");
+	glPushMatrix(); // GL_MODELVIEW is default
 	glScalef(1.0 / 100.0, 1.0 / 100.0, 1.0/100.0);
 
 	vertex v;
 
 	for(int i=1;i<100;i++)
 	{
-//		glColor3f(0.0, 0.0, 1.5*i);
 		glColor4f(1.0f, 0.0f, 0.0f, 0.5f);
-//		glColor4f(.23,.78,.32,0.1);
 		for(unsigned int j=0;j<particles[i].getSize();j++)
 		{
 			v = particles[i].getPosition(j);
@@ -70,17 +67,59 @@ void drawParticles(particle *particles)
 			glutSolidSphere (0.5, 20, 16);
 			glPopMatrix();
 		}
-//Trying to connect dots with lines
-/*
-		glBegin(GL_LINES); 
-	  	glLineWidth(4.0);
-		for(unsigned int j=0;j<particles[i].getSize();j++)
+	}
+	glPopMatrix();
+}
+
+void drawOffice(meshOBJ *obj)
+{
+	printf("drawing Office\n");
+	glPushMatrix(); // GL_MODELVIEW is default
+	glScalef(1.0 / 100.0, 1.0 / 100.0, 1.0/100.0);
+	vertex* normal;
+	vertex* vertex;
+	glBegin(GL_TRIANGLES); 
+	for(int i=0;i<1;i++)
+	{
+		for(unsigned int j=0;j<obj[i].getSize();j++)
 		{
-//			glColor3f(1.0f,0.0f,0.0f);
-//			v = particles[i].getPosition(j);
-//			glVertex3f(v.getX(),v.getY(),v.getZ());
+			normal = obj[i].getNormals(j);
+			vertex = obj[i].getVertices(j);
+			//Render 1 Face
+			glNormal3f(normal[0].getX(),normal[0].getY(),normal[0].getZ());
+			glVertex3f(vertex[0].getX(),vertex[0].getY(),vertex[0].getZ());
+			glNormal3f(normal[1].getX(),normal[1].getY(),normal[1].getZ());
+			glVertex3f(vertex[1].getX(),vertex[1].getY(),vertex[1].getZ());
+			glNormal3f(normal[2].getX(),normal[2].getY(),normal[2].getZ());
+			glVertex3f(vertex[2].getX(),vertex[2].getY(),vertex[2].getZ());
 		}
-		glEnd();
-*/	}
-//	glPopMatrix();
+	}
+	glEnd();
+	glPopMatrix();
+}
+
+void animate(particle *particles, int *iter)
+{
+	printf("animating\n");
+	glPushMatrix(); // GL_MODELVIEW is default
+	glScalef(1.0 / 100.0, 1.0 / 100.0, 1.0/100.0);
+	vertex v;
+	for(int i=2;i<3;i++)
+	{
+		glColor4f(1.0f, 0.0f, 0.0f, 0.5f);
+		if((unsigned int)*iter < particles[i].getSize())
+		{
+			v = particles[i].getPosition(*iter);
+			glPushMatrix(); // GL_MODELVIEW is default
+			glTranslatef(v.getX(),v.getZ(),v.getY());
+			glutSolidSphere (0.5, 20, 16);
+			glPopMatrix();
+			iter++;
+		}
+		else
+		{
+			*iter = -1;
+		}
+	}
+	glPopMatrix();
 }
